b2793687ed3d85e873c5ef4a2e9880d6ff28f949
i1i1 <vanyarybin1@live.ru>
2022-09-19 23:56:24 +0300

    Add updates for nodes

--- backend/test_utils/examples/bomb.rs
+++ backend/test_utils/examples/bomb.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 
+use chrono::prelude::*;
 use clap::Parser;
 use fdlimit::raise_fd_limit;
 use futures::stream::{FuturesUnordered, TryStreamExt};
-use test_utils::server::{CoreProcess, ShardProcess};
+use test_utils::server::{channels, CoreProcess, ShardProcess};
 use tokio::sync::Mutex;
+use zzz::prelude::*;
 
 #[derive(clap::Parser, Debug)]
 struct Args {
@@ -23,6 +25,30 @@ struct Args {
     host: String,
 }
 
+async fn send_msg(
+    shards: &[Arc<Mutex<(channels::ShardSender, channels::ShardReceiver)>>],
+    mut get_msg: impl FnMut(usize) -> serde_json::Value + Send + Copy + 'static,
+) {
+    let chunk_size = 100;
+    shards
+        .chunks(chunk_size)
+        .enumerate()
+        .map(|(i, chunk)| {
+            let shards = chunk.iter().cloned().collect::<Vec<_>>();
+            async move {
+                for (j, shard) in shards.into_iter().enumerate() {
+                    let n = chunk_size * i + j;
+                    shard.lock().await.0.send_json_text(get_msg(n)).unwrap();
+                }
+            }
+        })
+        .map(tokio::spawn)
+        .collect::<FuturesUnordered<_>>()
+        .try_collect::<Vec<_>>()
+        .await
+        .unwrap();
+}
+
 #[tokio::main]
 async fn main() {
     let Args {
@@ -37,23 +63,21 @@ async fn main() {
     let start = std::time::Instant::now();
 
     let shards = (0..shards / connections_per_task)
-        .map(|i| {
+        .map(|_| {
             let x = ShardProcess::from_host(format!("{host}:8001"));
             async move {
-                let res = x
-                    .connect_multiple_nodes(connections_per_task)
+                x.connect_multiple_nodes(connections_per_task)
                     .await
                     .map(|x| {
                         x.into_iter()
                             .map(Mutex::new)
                             .map(Arc::new)
                             .collect::<Vec<_>>()
-                    });
-                eprintln!("{i}");
-                res
+                    })
             }
         })
         .collect::<FuturesUnordered<_>>()
+        .progress()
         .try_collect::<Vec<_>>()
         .await
         .unwrap()
@@ -62,15 +86,12 @@ async fn main() {
         .collect::<Vec<_>>();
 
     let _feeds = (0..feeds / connections_per_task)
-        .map(|i| {
+        .map(|_| {
             let x = CoreProcess::from_host(format!("{host}:8000"));
-            async move {
-                let res = x.connect_multiple_feeds(connections_per_task).await;
-                eprintln!("Feed {i}");
-                res
-            }
+            async move { x.connect_multiple_feeds(connections_per_task).await }
         })
         .collect::<FuturesUnordered<_>>()
+        .progress()
         .try_collect::<Vec<_>>()
         .await
         .unwrap()
@@ -89,46 +110,43 @@ async fn main() {
 
     eprintln!("Sending. Initing took {:?}", start.elapsed());
 
-    for _ in 0..100 {
-        eprintln!("Sending");
-        let chunk_size = 100;
-        shards
-            .chunks(chunk_size)
-            .enumerate()
-            .map(|(i, chunk)| {
-                let shards = chunk.iter().cloned().collect::<Vec<_>>();
-                async move {
-                    for (j, shard) in shards.into_iter().enumerate() {
-                        let n = chunk_size * i + j;
-                        shard.lock().await.0.send_json_text(serde_json::json!({
-                            "id": 1, // message ID, not node ID. Can be the same for all.
-                            "ts":"2021-07-12T10:37:47.714666+01:00",
-                            "payload": {
-                                "authority":true,
-                                "chain":"Local Testnet",
-                                "config":"",
-                                "genesis_hash": "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3",
-                                "implementation":"Substrate Node",
-                                "msg":"system.connected",
-                                "name": format!("Alice {n}"),
-                                "network_id":"12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp",
-                                "startup_time":"1625565542717",
-                                "version":"2.0.0-07a1af348-aarch64-macos"
-                            }
-                        })).unwrap();
-                    }
-                }
+    // loop {
+    eprintln!("Sending new nodes");
+    send_msg(&shards, |n| {
+        serde_json::json!({
+            "id": 1,
+            "ts": Utc::now().to_rfc3339(),
+            "payload": {
+                "authority": true,
+                "chain": "Local Testnet",
+                "config": "",
+                "genesis_hash": "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3",
+                "implementation": "Substrate Node",
+                "msg": "system.connected",
+                "name": format!("Alice {n}"),
+                "network_id": "12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp",
+                "startup_time": "1625565542717",
+                "version": "2.0.0-07a1af348-aarch64-macos"
+            }
+        })
+    }).await;
+    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+
+    for i in 0.. {
+        eprintln!("Sending update {i}");
+        send_msg(&shards, move |_| {
+            serde_json::json!({
+                "id": 1,
+                "ts": Utc::now().to_rfc3339(),
+                "payload": {
+                    "bandwidth_download": i,
+                    "bandwidth_upload": 2 * i,
+                    "msg": "system.interval",
+                    "peers": 1,
+                },
             })
-            .map(tokio::spawn)
-            .collect::<FuturesUnordered<_>>()
-            .try_collect::<Vec<_>>()
-            .await.unwrap();
-
-        tokio::time::sleep(std::time::Duration::from_millis(500)).await;
-    }
-
-    for _ in 0..1_000 {
-        // 100secs
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        })
+        .await;
+        tokio::time::sleep(std::time::Duration::from_secs(6)).await;
     }
 }
